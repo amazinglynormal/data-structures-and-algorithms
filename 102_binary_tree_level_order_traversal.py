698c12aba18f0afdea68d1d63b5c4723aaa150bb
Stephen <stephenbrady365@gmail.com>
2023-09-21 19:59:31 +0100

    leetcode #102 - medium

---
+++ 102_binary_tree_level_order_traversal.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def levelOrder(root):
+    levels = []
+
+    if not root:
+        return levels
+
+    queue = deque()
+
+    queue.append(root)
+
+    while len(queue) > 0:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        levels.append(level)
+
+    return levels
