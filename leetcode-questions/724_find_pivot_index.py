b4b3d86ed6ce255531828ba4275a090a5e832d15
Stephen <stephenbrady365@gmail.com>
2023-11-06 09:23:38 +0000

    leetcode #724 - easy

---
+++ leetcode-questions/724_find_pivot_index.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+
+        total = sum(nums)
+
+        leftSum = 0
+
+        for i in range(len(nums)):
+            if leftSum == (total - leftSum - nums[i]):
+                return i
+            leftSum += nums[i]
+
+        return -1
